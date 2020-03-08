c4d6bbebbebbd319228bf2ba4508beadbb2e7e67
baimadev <rupeng.xia@fenrir-inc.com.cn>
2020-03-08 23:00:50 +0800

    优化gprImageView

--- app/src/main/java/com/example/lifecycle/ui/customview/ColorDialog.kt
+++ app/src/main/java/com/example/lifecycle/ui/customview/ColorDialog.kt
@@ -1,5 +1,6 @@
 package com.example.lifecycle.ui.customview
 
+import android.app.Activity
 import android.app.Dialog
 import android.content.Context
 import android.graphics.Bitmap
@@ -8,20 +9,12 @@ import android.view.Gravity
 import android.widget.ImageView
 import android.widget.SeekBar
 import androidx.coordinatorlayout.widget.CoordinatorLayout
-import com.example.lifecycle.GPRImageView
 import com.example.lifecycle.R
 import com.example.lifecycle.utils.ImageHelper
 import com.example.lifecycle.utils.SharedPrefModel
 import kotlinx.android.synthetic.main.dialog_color.*
 import java.util.*
-
-
 class ColorDialog (mContext: Context, val layoutId :Int,val gprImageView: GPRImageView):Dialog(mContext, R.style.MyDialog) {
-
-
-    private var mImageView: ImageView? = null
-
-    private val mBitmap: Bitmap? = null
 
     companion object{
         const val MAX_VALUE = 255
@@ -29,15 +22,17 @@ class ColorDialog (mContext: Context, val layoutId :Int,val gprImageView: GPRIma
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        window!!.setGravity(Gravity.CENTER)
+        window!!.setGravity(Gravity.CENTER_VERTICAL)
         setContentView(layoutId)
-
         val lp = window!!.attributes
+            //   val display = (context as Activity).windowManager.defaultDisplay
         //宽度
-        lp.width = CoordinatorLayout.LayoutParams.MATCH_PARENT
+        lp.width = CoordinatorLayout.LayoutParams.WRAP_CONTENT
         lp.height = CoordinatorLayout.LayoutParams.WRAP_CONTENT
-        lp.gravity = Gravity.CENTER
+        lp.y = 0
+        lp.y = 0
+
+       // lp.gravity = Gravity.CENTER
         window!!.attributes = lp
         //点击外部不消失
         setCanceledOnTouchOutside(false)
@@ -48,11 +43,9 @@ class ColorDialog (mContext: Context, val layoutId :Int,val gprImageView: GPRIma
         val colorArray = listOf<Int>(R.drawable.ic_arrow_drop_down_black_24dp,R.drawable.brvah_sample_footer_loading_progress)
         val adapter = SpinnerAdapter(context,colorArray,R.layout.item_spinner)
         //sp_sd.setAdapter(adapter)
-
         val dataset: List<String> =
             LinkedList(Arrays.asList("One", "Two", "Three", "Four", "Five"))
         sp_sd.attachDataSource(dataset)
-
         //seekbar
         //饱和度
         sb_bhd.let {
@@ -64,13 +57,9 @@ class ColorDialog (mContext: Context, val layoutId :Int,val gprImageView: GPRIma
                     SharedPrefModel.mSaturationPos =progress
                     setImageview()
                 }
-
                 override fun onStartTrackingTouch(seekBar: SeekBar?) {
-
                 }
-
                 override fun onStopTrackingTouch(seekBar: SeekBar?) {
-
                 }
             })
         }
@@ -119,14 +108,10 @@ class ColorDialog (mContext: Context, val layoutId :Int,val gprImageView: GPRIma
         image_done.setOnClickListener {
             this.dismiss()
         }
-
-
     }
 
     fun setImageview(){
         val bitmap = ImageHelper.handleImageEffec(gprImageView.getCurrentBitmap(), SharedPrefModel.mHue, SharedPrefModel.mSaturation, SharedPrefModel.mLum)
         gprImageView.setImageBitmap(bitmap)
     }
-
-
 }
--- app/src/main/java/com/example/lifecycle/ui/fragment/TopFragment.kt
+++ app/src/main/java/com/example/lifecycle/ui/fragment/TopFragment.kt
@@ -1,12 +1,9 @@
 package com.example.lifecycle.ui.fragment
 
 import android.content.Intent
-import android.graphics.Color
 import android.util.Log
-import com.example.lifecycle.GPRImageView
+import com.example.lifecycle.ui.customview.GPRImageView
 import com.example.lifecycle.R
-import com.example.lifecycle.base.CloseMode
-import com.example.lifecycle.base.CustomToolbar
 import com.example.lifecycle.databinding.FragmentTopBinding
 import com.example.lifecycle.ui.activity.SplashActivity
 import com.example.lifecycle.ui.customview.ColorDialog
@@ -15,6 +12,7 @@ import com.jakewharton.rxbinding2.view.RxView
 import com.photo.ui.fragment.base.BindingFragment
 import com.photo.utils.Constants
 import com.photo.utils.netProgressDialog
+import com.photo.utils.switchThread
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
@@ -24,19 +22,13 @@ import java.io.File
 
 class TopFragment : BindingFragment<FragmentTopBinding, TopViewModel>(TopViewModel::class.java, R.layout.fragment_top){
 
-    lateinit var gprImage:GPRImageView
+    lateinit var gprImage: GPRImageView
 
     override fun initView() {
         gprImage = matrix
         gprImage.let {
             gprImage = matrix
         }
-//        tool_bar.let {
-//            it.doOnClickCloseButton = {
-//                finish()
-//                startActivity(Intent(context,SplashActivity::class.java))
-//            }
-//        }
 
         RxView.clicks(binding.imageClose)
             .doOnNext {
@@ -49,7 +41,6 @@ class TopFragment : BindingFragment<FragmentTopBinding, TopViewModel>(TopViewMod
         RxView.clicks(binding.imageColor)
             .doOnNext {
                 val dialog = ColorDialog(context!!,R.layout.dialog_color,gprImage)
-                Log.d("xia","click")
                 dialog.show()
             }
             .bindLife()
@@ -58,18 +49,21 @@ class TopFragment : BindingFragment<FragmentTopBinding, TopViewModel>(TopViewMod
 
     override fun initData() {
         val rd3File = File(Constants.RD3)
-
         Single.just(FileUtil.readFileToMatrix2(rd3File,1000))
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .netProgressDialog(context!!)
             .doOnSuccess {
                 // set the default image display type
-                gprImage.setGPRData(it)
+                gprImage.initImageBitmap(it)
+                    .switchThread()
+                    .netProgressDialog(context!!)
+                    .doOnSuccess {
+                        gprImage.setImageBitmap(it)
+                    }
+                    .bindLife()
                 gprImage.invalidate()
-
             }
             .bindLife()
-
     }
 }
